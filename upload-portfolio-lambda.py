cdf4f4f1fc44b83fe1e805adbc69ba9259c24bc6
ianbunn <ianrichardbunn@gmail.com>
2019-04-16 22:37:07 -0500

    updating inputArtifact name

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
         
         if job:
             for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "BuildArtifact":
+                if artifact["name"] == "SourceArtifact":
                     location = artifact["location"]["s3Location"]
         
         print "Building portfolio from " + str(location)
