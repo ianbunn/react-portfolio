0ce7d1e9ead81f621c8c4e90023ee503d99da69d
ianbunn <ianrichardbunn@gmail.com>
2019-04-17 13:53:09 -0500

    adding changes to lambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
@@ -11,7 +12,7 @@ def lambda_handler(event, context):
 
     try:
         
-        s3 = boto3.resource('s3')
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
         
         portfolio_bucket = s3.Bucket('portfolio.ianbunn.studio')
         build_bucket = s3.Bucket('portfoliobuild.ianbunn.studio')
@@ -25,7 +26,7 @@ def lambda_handler(event, context):
                 portfolio_bucket.upload_fileobj(obj, nm,
                     ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        print 'Portfolio has been deployed!'
+        print ("Portfolio has been deployed!")
         topic.publish(Subject="Portfolio deployment completed", Message="https://portfolio.ianbunn.studio")
     except:
         topic.publish(Subject="Portfolio deployment FAILED", Message="The portfolio deployment failed. Please review now and try again.")
